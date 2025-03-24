fcdcc71a1d2bc23bfcc3d31ea73112eb02ff3308
florichdev <furut4yo@gmail.com>
2025-03-24 17:44:13 +0300

    last version 24.03.2025

---
+++ Pract1_Florich_I223/AuthWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Pract1_Florich_I223.Logic;
+using Pract1_Florich_I223.dbContext;
+
+namespace Pract1_Florich_I223
+{
+    public partial class AuthWindow : Window
+    {
+        private IAuthService _authService;
+        private ShopDB_FlorichEntities dbContext;
+
+        public AuthWindow()
+        {
+            InitializeComponent();
+            dbContext = new ShopDB_FlorichEntities();
+            _authService = new AuthService();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = tbxlogin.Text;
+            string Pass = tbxPass.Text;
+
+
+            if (_authService.CheckData(Login,Pass))
+            {
+                ProductDataGrid dataGrid = new ProductDataGrid();
+                dataGrid.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка, проверьте правильность введённых данных в полях");
+            }
+
+        }
+    }
+}
---
+++ Pract1_Florich_I223/Logic/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pract1_Florich_I223.dbContext;
+using Pract1_Florich_I223.Model;
+
+namespace Pract1_Florich_I223.Logic
+{
+    public class AuthService : IAuthService
+    {
+        private ShopDB_FlorichEntities dbContext = new ShopDB_FlorichEntities();
+
+        public bool CheckData(string Login, string Pass)
+        { 
+
+            var user = dbContext.Users.FirstOrDefault(u => u.Login == Login && u.Pass == Pass);
+
+            if (user != null && user.Pass == Pass)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
