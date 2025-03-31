a512f4f39a066c06e390d239d428d217cf1a4e82
florichdev <furut4yo@gmail.com>
2025-03-31 17:36:50 +0300

    add roles

---
+++ Pract1_Florich_I223/Logic/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pract1_Florich_I223.dbContext;
+using Pract1_Florich_I223.Model;
+using System.Data.Entity;
+using Pract1_Florich_I223.Logic;
+
+namespace Pract1_Florich_I223.Logic
+{
+    public class AuthService : IAuthService
+    {
+        private RoleService roleService;
+        private ShopDB_FlorichEntities dbContext = new ShopDB_FlorichEntities();
+
+        public bool CheckData(string Login, string Pass)
+        {
+            var user = dbContext.Users.Include(u => u.Role).FirstOrDefault(u => u.Login == Login && u.Pass == Pass);
+
+            if (user != null && user.Pass == Pass)
+            {
+                ProductDataGrid dataGrid = new ProductDataGrid(user);
+                dataGrid.Show();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ Pract1_Florich_I223/Logic/RoleService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Pract1_Florich_I223.dbContext;
+using Pract1_Florich_I223.Logic;
+using Pract1_Florich_I223.Model;
+using System.Windows;
+
+namespace Pract1_Florich_I223.Logic
+{
+    public class RoleService
+    {
+        private ShopDB_FlorichEntities dbContext = new ShopDB_FlorichEntities();
+        public Users currentUser;
+
+        public string GetUserRole(Users user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "Пользователь не найден");
+            }
+
+            using (var dbContext = new ShopDB_FlorichEntities())
+            {
+                var currentUser = dbContext.Users.Include("Role").FirstOrDefault(u => u.RoleID == user.RoleID);
+
+                if (currentUser != null && currentUser.Role != null)
+                {
+                    return currentUser.Role.RoleName;
+                }
+                else
+                {
+                    return "Роль не определена";
+                }
+            }
+        }
+    }
+}
